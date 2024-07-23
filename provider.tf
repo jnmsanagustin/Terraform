53dbbb2390e0855c88f4efa3e82ad5261da8ae25
JNM <jnmsanagustin@gmail.com>
2024-07-23 17:32:33 +0800

    Remove Github Provider

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
 provider "aws" {
     region     = "ap-southeast-1"
 }
-
-terraform {
-  required_providers {
-    github = {
-      source = "integrations/github"
-      version = "6.2.3"
-    }
-  }
-}
